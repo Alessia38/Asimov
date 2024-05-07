8d4e384e35c783ef1c8c5f4dc0c47b7845ea605b
pierre MICHAUD <pierre.michaud3806@gmail.com>
2024-05-07 11:50:11 +0200

    deconnexion

--- index.js
+++ index.js
@@ -56,8 +56,8 @@ app.get('/accueil', function(req, res) {
 // Utilisation des routes
 
 app.use('/eleve', eleveRoutes);
-/*
 app.use('/enseignant', enseignantRoutes);
+/*
 app.use('/proviseur', proviseurRoutes);
 app.use('/secretariat', secretariatRoutes);*/
 app.use('/connexion', connexionRoutes);
